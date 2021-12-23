f528d1c28cdd921ad86b6e983aff36723464609d
sravya0413 <96100397+sravya0413@users.noreply.github.com>
2021-12-23 12:29:24 +0530

    Update App.java

--- src/main/java/Mindtree/simple/App.java
+++ src/main/java/Mindtree/simple/App.java
@@ -4,10 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
 	public static void main(String[] args) throws InterruptedException {
